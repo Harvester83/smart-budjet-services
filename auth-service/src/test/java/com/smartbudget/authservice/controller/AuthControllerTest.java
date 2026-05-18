319bc71be6b7e34b702d4daddffaeb50190aaeea
Rashad <rashad.suleymanov83@gmail.com>
2026-05-18 23:50:38 +0400

    test(auth-service): add AuthControllerTest

--- auth-service/src/test/java/com/smartbudget/authservice/controller/AuthControllerTest.java
+++ auth-service/src/test/java/com/smartbudget/authservice/controller/AuthControllerTest.java
@@ -4,14 +4,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.smartbudget.authservice.dto.AuthResponse;
 import com.smartbudget.authservice.dto.LoginRequest;
 import com.smartbudget.authservice.service.AuthService;
+
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -21,7 +26,7 @@ class AuthControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private AuthService authService;
 
     @Autowired
